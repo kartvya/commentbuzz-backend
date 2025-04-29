a9691421e4bf0f12553e4c15bef4ffdb208b7213
Kartvya Acharya <68657182+kartvya@users.noreply.github.com>
2025-04-29 17:17:04 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use("/api/auth", authRoutes);
 app.get("/api/profile", isAuthenticated, getProfile);
 
 app.get("/", (req, res) => {
-  res.send("✅ API is running...");
+  res.send("API is running...");
 });
 
 // Start server
